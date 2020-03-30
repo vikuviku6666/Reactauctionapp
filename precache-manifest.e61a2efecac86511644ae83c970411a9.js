9d5d1da7b08a8b031fd590a551cfe3cf722c8eb8
vinay <vk6725@gmail.com>
2020-03-30 18:49:02 +0200

    Updates

---
+++ precache-manifest.e61a2efecac86511644ae83c970411a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a33535577464b1b37dd70a73c2acf0f",
+    "url": "/Reactauctionapp/index.html"
+  },
+  {
+    "revision": "8f3fbc9d04e61b756489",
+    "url": "/Reactauctionapp/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "5d6330be42defad38e1a",
+    "url": "/Reactauctionapp/static/css/main.c0ca279b.chunk.css"
+  },
+  {
+    "revision": "8f3fbc9d04e61b756489",
+    "url": "/Reactauctionapp/static/js/2.a2d52d05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reactauctionapp/static/js/2.a2d52d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d6330be42defad38e1a",
+    "url": "/Reactauctionapp/static/js/main.642c3443.chunk.js"
+  },
+  {
+    "revision": "ffa9276232e4c66f6729",
+    "url": "/Reactauctionapp/static/js/runtime-main.4f6d760a.js"
+  }
+]);
